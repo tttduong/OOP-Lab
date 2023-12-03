aee03b43c2c9fbef99634e61160c6f5483ec638a
Thuy Duong <100646834+tttduong@users.noreply.github.com>
2023-12-03 13:55:01 +0700

    Up TestRetangle

---
+++ TestRectangle.java
@@ -0,0 +1,19 @@
+public class TestRectangle {
+    public static void main (String[] args){
+
+        Rectangle rectangle1 = new Rectangle (3,4);
+        Rectangle rectangle2 = new Rectangle (5,4);
+        Rectangle rectangle3 = new Rectangle (6,3);
+        Rectangle rectangle4 = new Rectangle (9,4);
+        Rectangle rectangle5 = new Rectangle (3,10); 
+
+        rectangle1.visualize();
+        rectangle2.visualize();
+        rectangle3.visualize();
+        rectangle4.visualize();
+        rectangle5.visualize();
+
+
+    }
+    
+}
